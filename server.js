d22124a9ecfcdd668f9c591e5ecab2ad249e7bdc
Kap0812 <dortegakap0812@gmail.com>
2025-11-25 17:50:32 -0700

    MVC

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const baseController = require("./controllers/baseController")
 /* ***********************
  * view and engine templates
  *************************/
@@ -21,7 +22,8 @@ app.set("layout", "./layouts/layout") // not at views root
  *************************/
 app.use(static)
 //index route
-app.get("/", function(req, res){
+app.get("/", baseController.buildHome)
+  /* function(req, res){
   res.render("index", {title: "home"})
 })
 /* ***********************
