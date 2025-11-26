960c727fcf1203de763acc50ffbc68968338ca94
Kap0812 <dortegakap0812@gmail.com>
2025-11-26 13:57:19 -0700

    classification view

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
 /* ***********************
  * view and engine templates
  *************************/
@@ -23,6 +24,8 @@ app.set("layout", "./layouts/layout") // not at views root
 app.use(static)
 //index route
 app.get("/", baseController.buildHome)
+// Inventory routes
+app.use("/inv", inventoryRoute)
   /* function(req, res){
   res.render("index", {title: "home"})
 })
