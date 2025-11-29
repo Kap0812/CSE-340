76ff85da3e954a54190ce252327b722fbd558d1b
Kap0812 <dortegakap0812@gmail.com>
2025-11-29 10:58:41 -0700

    assigmnet 4

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -2,12 +2,27 @@
 const express = require("express")
 const router = new express.Router() 
 const invController = require("../controllers/invController")
-const utilities = require("../utilities/");
+const utilities = require("../utilities/")
 
 // Route to build inventory by classification view
-router.get("/type/:classificationId",utilities.handleErrors(invController.buildByClassificationId));
+router.get("/type/:classificationId", utilities.handleErrors(invController.buildByClassificationId));
 
 // Route to build inventory item detail view
 router.get("/detail/:invId", utilities.handleErrors(invController.buildByInventoryId));
+
+// Route to build inventory management view
+router.get("/", utilities.handleErrors(invController.buildManagement));
+
+// Route to build add classification view
+router.get("/add-classification", utilities.handleErrors(invController.buildAddClassification));
+
+// Route to process add classification (POST)
+router.post("/add-classification", utilities.handleErrors(invController.addClassification));
+
+// Route to build add inventory view
+router.get("/add-inventory", utilities.handleErrors(invController.buildAddInventory));
+
+// Route to process add inventory (POST)
+router.post("/add-inventory", utilities.handleErrors(invController.addInventory));
 
 module.exports = router;
